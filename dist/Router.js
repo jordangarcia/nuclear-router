a3e904c41dee16b31df1388ad6def1c9d8fc7eb3
Matt Carroll <matt.carroll@optimizely.com>
2017-01-17 15:46:07 -0800

    Update built file in dist

--- dist/Router.js
+++ dist/Router.js
@@ -226,7 +226,11 @@ var Router = function () {
         this.__currentCanonicalPath = canonicalPath;
 
         if (this.onRouteStart && mode !== 'replace') {
-          this.onRouteStart();
+          this.onRouteStart({
+            fromPath: this.__fromPath || 'PAGE LOAD',
+            startTime: this.__startTime,
+            context: ctx
+          });
         }
 
         this.__runHandlers(route.handlers, ctx, function () {
